50d3a4e6fa36a91d229e8c3244b6abea7f60d98f
Frank Uzoka <Frankuzoka@hotmail.com>
2020-05-12 10:24:00 -0400

    Import spring boot

---
+++ ConsumingAPI/src/main/java/com/Consuming/ConsumingAPI/ConsumingApiApplication.java
@@ -0,0 +1,13 @@
+package com.Consuming.ConsumingAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingApiApplication.class, args);
+	}
+
+}
---
+++ ConsumingAPI/src/test/java/com/Consuming/ConsumingAPI/ConsumingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Consuming.ConsumingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
